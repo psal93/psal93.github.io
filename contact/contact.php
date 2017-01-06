5a316ab66494123be8dfd67b78a93a27ed01b2f8
Patrick Salembier <patsalembier@gmail.com>
2017-01-05 17:19:17 -0800

    Add files via upload

---
+++ contact/contact.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Patrick Salembier's contact</title>
+  <meta charset="utf-8"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="contact.css">
+</head>
+<body>
+	<header class="container">
+  	<div class="row">
+      <h1 class="col-sm-4"><a href="../index.html">P.S.</a></h1>
+      <nav class="col-sm-8 text-right">
+        <p><a href="../projects/projects.html">Projects</a></p>
+        <p><a href="../webMaps/webMaps.html">Web Maps</a></p>
+        <p><a href="../TechResume2016.pdf">Resume PDF Download </a></p>
+        <p><a href="contact.php">Contact</a></p>
+      </nav>
+    </div>
+  </header>
+  <section class="container">
+    <div class="row">
+      <figure class="col-sm-12">
+        <p>I can be reached at patsalembier@gmail.com</p>
+      </figure>
+      <figure class="col-sm-5">
+        <p>Don't hesitate to contact me, I'm always up to chat!</p>
+      </figure>
+    </div>
+  </section>
+  <footer class="container">
+    <div class="row">
+      <p class="col-sm-4">&copy; 2016 Patrick Salembier</p>
+      <ul class="col-sm-8">
+        <li class="col-sm-1">
+        	  <a href="https://github.com/psal93">
+              <img src="gitHubLogo_Big.png">
+            </a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+
+  <script
+    src="https://code.jquery.com/jquery-3.1.1.min.js"
+    integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+    crossorigin="anonymous"></script>
+  <script src='main.js'></script>
+
+</body>
+</html>
